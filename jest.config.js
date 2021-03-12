ff1d6b21af7fc19d98ce0773526720016805d902
Marek Firlejczyk <marekfirlejczyk@gmail.com>
2021-03-12 10:03:38 +0100

    chore: improved jest & ts conifg

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
-  globals: {},
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '^.+\\js$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
-  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  moduleFileExtensions: ['vue', 'js', 'ts'],
 }
